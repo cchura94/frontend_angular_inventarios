8bf2d2520a2eef99395092928b99d766ad044b18
Cristian <cchura.cpc@gmail.com>
2026-03-05 21:08:58 -0400

    componente login con laravel

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './login.scss',
 })
 export class Login {
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  token: string = "";
+
+  authService = inject(AuthService)
+
+  funIngresarConLaravel(){
+    this.authService.funLoginConectarConBackendLaravel(this.loginForm.value).subscribe(
+      (res: any) => {
+        console.log("CORRECTO: ", res);
+        this.token = res.access_token;
+      }
+    )
+  }
 
 }
---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  urlBase = environment.servidor1
+
+  http = inject(HttpClient);
+
+  funLoginConectarConBackendLaravel(credenciales: any){
+    return this.http.post(`${this.urlBase}/api/v1/auth/login`, credenciales)
+  }
+
+}
