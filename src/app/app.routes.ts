0956cdf765b43aa7602ce9b20ccc62caa48acf8b
Cristian <cchura.cpc@gmail.com>
2026-03-05 20:37:01 -0400

    rutas + navegacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { WebLayout } from './layout/web-layout/web-layout';
+import { Inicio } from './web/inicio/inicio';
+import { Nosotros } from './web/nosotros/nosotros';
+import { Servicios } from './web/servicios/servicios';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: WebLayout,
+        children: [
+            { path: "", component: Inicio },
+            { path: "nosotros", component: Nosotros },
+            { path: "servicios", component: Servicios },
+            {
+                path: "auth",
+                loadChildren: () => import('./auth/auth-module').then(m => m.AuthModule)
+            }
+        ]
+    },
+];
