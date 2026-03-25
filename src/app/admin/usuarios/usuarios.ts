f2619e4a58a0218fbcd99dd2a0bbddd24dedd628
Cristian <cchura.cpc@gmail.com>
2026-03-24 21:10:11 -0400

    crud usuarios

--- src/app/admin/usuarios/usuarios.ts
+++ src/app/admin/usuarios/usuarios.ts
@@ -1,27 +1,33 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import UsuarioInterface from '../../core/interfaces/UsuarioInterface';
 import { UsuarioService } from '../../core/services/usuario.service';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { email } from '@angular/forms/signals';
 import { TableModule } from 'primeng/table';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { SelectModule } from 'primeng/select';
+
 
 @Component({
   selector: 'app-usuarios',
-  imports: [ReactiveFormsModule, TableModule, DialogModule, ButtonModule],
+  imports: [ReactiveFormsModule, FormsModule, TableModule, DialogModule, ButtonModule, InputTextModule, PasswordModule, SelectModule],
   templateUrl: './usuarios.html',
   styleUrl: './usuarios.scss',
 })
 export class Usuarios implements OnInit{
   
   visible: boolean = false;
+  selectedCiudad: string = "" ;
 
     
 
   usuarios = signal<UsuarioInterface[]>([]);
   usuarioService = inject(UsuarioService);
-  user_id = signal(-1);
+  user_id = signal<number>(-1);
+  cargando = signal<boolean>(true);
 
   userForm = new FormGroup({
     name: new FormControl(''),
@@ -34,9 +40,11 @@ export class Usuarios implements OnInit{
   }
 
   funListarUsuario(){
+    this.cargando.set(true);
     this.usuarioService.index().subscribe(
       (res: any) => {
         this.usuarios.set(res.data);
+        this.cargando.set(false);
       },
       error => {
         // alert("Error al obtener los usuarios");
@@ -44,12 +52,25 @@ export class Usuarios implements OnInit{
     )
   }
 
+  funObtenerLocalidades(){
+    this.funListarUsuario();
+  }
+
+  editProduct(prod: any){
+    // this.userForm.set
+    this.userForm.patchValue({name: prod.name, email: prod.email});
+    this.user_id.set(prod.id)
+    this.visible = true
+  }
+  deleteProduct(prod: any){
+
+  }
+
   showDialog() {
     this.visible = true;
 }
 
   funGuardarUsuario(){
-
     if(this.user_id() > -1){
       const userdata = { email: this.userForm.value.email || '', password: this.userForm.value.password || '', name: this.userForm.value.name || ''}
       this.usuarioService.update(this.user_id(), userdata).subscribe(
@@ -75,6 +96,7 @@ export class Usuarios implements OnInit{
         }
       )
     }
+    this.visible = false;
   }
 
   funEditarUsuario(u: any){
@@ -89,6 +111,8 @@ export class Usuarios implements OnInit{
       email: new FormControl(u.email, [Validators.required, Validators.email]),
       password: new FormControl(u.password)
     });
+
+    this.visible = false
 
     // this.userForm.value.email = u.email;
     // this.userForm.value.name = u.name;
